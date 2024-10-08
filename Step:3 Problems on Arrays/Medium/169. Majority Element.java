05dab67d7fbb1da6857565e2e00a43a4eb8cf980
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-10-08 15:58:47 +0530

    Flipkart Accolite Amazon Microsoft D-E-Shaw Google Nagarro Atlassian
    https://leetcode.com/problems/majority-element/
    https://www.geeksforgeeks.org/problems/majority-element-1587115620/1

---
+++ Step:3 Problems on Arrays/Medium/169. Majority Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++) {
+            int count = 1;
+            for(int j=i+1;j<n;j++) {
+                if(nums[i] == nums[j]) count++;
+            }
+            if(count > n/2) return nums[i];
+        }
+        return -1;
+    }
+}
