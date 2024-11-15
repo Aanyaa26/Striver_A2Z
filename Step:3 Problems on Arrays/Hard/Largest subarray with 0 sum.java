7d6aa430b09d9078666247423438a0d2b2546b0a
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-11-15 21:57:35 +0530

    prefix sum & hashmaps
    https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1?category%5B%5D=Hash&company%5B%5D=Amazon&page=1&query=category%5B%5DHashcompany%5B%5DAmazonpage1company%5B%5DAmazoncategory%5B%5DHash&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-subarray-with-0-sum

---
+++ Step:3 Problems on Arrays/Hard/Largest subarray with 0 sum.java
@@ -0,0 +1,24 @@
+Date: 15.11.24
+class Solution {
+    int maxLen(int arr[]) {
+        HashMap<Integer, Integer> map = new HashMap <>();
+        int max=0;
+        int sum =0;
+        
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i];
+            if(sum==0){
+                max = i +1;
+            }
+            else{
+                if(map.get(sum)!=null){
+                    max = Math.max(max , i-map.get(sum));
+                }
+                else{
+                    map.put(sum,i);
+                }
+            }
+        }
+        return max;
+    }
+}
