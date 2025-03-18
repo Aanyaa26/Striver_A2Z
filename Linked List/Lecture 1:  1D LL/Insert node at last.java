4125ca59293218ce7de17f4b3b95c019c46ac2ed
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-03-19 02:13:36 +0530

    basic

---
+++ Linked List/Lecture 1:  1D LL/Insert node at last.java
@@ -0,0 +1,15 @@
+Date: 19.03.25
+  class Solution {
+    static Node constructLL(int arr[]) {
+        // initialising head of the ll
+        Node head = new Node(arr[0]); // assigned the head with first element of the ll
+        Node mover = head; // As head must not get tamperred , hence stored in a pointer type variable which is note fixed and hence changes as soon as pointer moves forward
+        
+        for(int i=1; i<arr.length; i++){
+            Node temp = new Node(arr[i]); // assigning temp with ith element of array
+            mover.next = temp; // pointing mover (initially which was head) to the next temp node
+            mover = temp; // making mover pointer assigned to the current temp node
+        }
+        return head;
+    }
+}
