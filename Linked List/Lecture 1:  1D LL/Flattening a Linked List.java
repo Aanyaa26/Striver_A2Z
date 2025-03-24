c670fbf35b8deee0af420b0bd5f2d091e7429d08
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-03-24 22:40:25 +0530

    Create Flattening a Linked List.java

---
+++ Linked List/Lecture 1:  1D LL/Flattening a Linked List.java
@@ -0,0 +1,127 @@
+Date: 24.03.25
+//{ Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+    Node bottom;
+
+    Node(int x) {
+        data = x;
+        next = null;
+        bottom = null;
+    }
+}
+
+public class GFG {
+    // Function to print the flattened linked list
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.bottom;
+        }
+        System.out.println();
+    }
+
+    // Create a linked list where `next` pointers connect the heads of multiple lists
+    static Node createLinkedList(List<Node> lists) {
+        Node head = null, temp = null;
+
+        for (Node list : lists) {
+            if (head == null) {
+                head = list;
+                temp = list;
+            } else {
+                temp.next = list;
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read number of test cases
+        int t = Integer.parseInt(scanner.nextLine());
+
+        while (t-- > 0) {
+            // Read number of linked lists
+            int n = Integer.parseInt(scanner.nextLine());
+            List<Node> lists = new ArrayList<>();
+
+            // Read each linked list
+            for (int i = 0; i < n; i++) {
+                String line = scanner.nextLine();
+                String[] values = line.split(" ");
+                Node head = null, temp = null;
+
+                for (String value : values) {
+                    Node newNode = new Node(Integer.parseInt(value));
+                    if (head == null) {
+                        head = newNode;
+                        temp = head;
+                    } else {
+                        temp.bottom = newNode; // Corrected from `.next` to `.bottom`
+                        temp = temp.bottom;
+                    }
+                }
+
+                lists.add(head);
+            }
+
+            // Create a linked list connecting all heads using `next` pointers
+            Node list = createLinkedList(lists);
+
+            // Flatten the linked list using the solution
+            Solution sol = new Solution();
+            Node flattenedList = sol.flatten(list);
+
+            // Print the flattened list
+            printList(flattenedList);
+            System.out.println("~");
+        }
+
+        scanner.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to flatten a linked list
+    Node merge (Node a, Node b){
+        Node temp = new Node (0) ; // creating a dummy node to start and merge the nodes
+        Node res = temp; // to return the merge ll
+        
+        while(a!= null && b!= null){
+            if(a.data<b.data){
+                temp.bottom =a;
+                temp = temp.bottom;
+                a = a.bottom;
+            }else{
+                temp.bottom = b;
+                temp = temp.bottom;
+                b = b.bottom;
+            }
+        }
+        if(a!=null)temp.bottom = a;
+        else temp.bottom =b;
+        return res.bottom;
+    }
+    Node flatten(Node root) {
+        // code here
+        if(root==null || root.next == null){
+            return root;
+        }
+        
+        root.next = flatten(root.next);
+        root = merge(root,root.next);
+        
+        return root;
+    }
+}
