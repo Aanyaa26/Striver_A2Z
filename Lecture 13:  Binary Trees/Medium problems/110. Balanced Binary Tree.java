625d56cd920c56462f4d872d41f0d1f4d3a8e8c0
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-04-03 11:00:45 +0530

    imp- similar to height of tree

---
+++ Lecture 13:  Binary Trees/Medium problems/110. Balanced Binary Tree.java
@@ -0,0 +1,31 @@
+Date:03.04.25
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root)!=-1;
+    }
+    int dfs(TreeNode root){
+        if(root==null)return 0;
+        int left = dfs(root.left);
+        if(left==-1)return -1;
+        int right = dfs(root.right);
+        if(right==-1)return -1;
+
+        if(Math.abs(left-right)>1)return -1;
+        return 1+Math.max(left,right);
+    }
+}
